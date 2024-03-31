fceaa5ae4813bbfae722f6e2107cf380ee1cd465
Felipe <felipe.arciniegas@utp.edu.co>
2024-03-31 18:43:17 -0500

    Updated 2.0

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI , Body , Path , Query , Request , HTTPException , De
 from fastapi.responses import HTMLResponse , JSONResponse
 from typing import List, Optional
 from pydantic import BaseModel, Field
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPBearer
+#from config.database import Session, engine, Base
+#from models.movie import Movie as MovieModel
 
+# Creando una instancia de la clase FastAPI
 app = FastAPI()
+#Base.metadata.create_all(bind=engine)
 
+# Cambios a la documentacion
 app.title = "Mi Super Api"
-app.version = "V1.0.0"
+app.version = "V2.0.0"
+
+class User(BaseModel):
+    email: str
+    password: str
 
 class Movie(BaseModel): 
     id: Optional[int] = None
@@ -15,6 +26,23 @@ class Movie(BaseModel):
     year: int = Field(default=2024, le=2024)
     rating: float = Field(ge=0, le=10, default=10)
     category: str = Field(default="Comedia", min_length=5, max_length=20)
+
+    # Configuracion de la documentacion
+    class Config:
+        model_config = {
+        "json_schema_extra": {
+                "examples": [
+                    {
+                        "id": 1,
+                        "title": "Mi Pelicula",
+                        "overview": "Descripcion de la pelicula",
+                        "year": 2022,
+                        "rating": 9.9,
+                        "category": "Acción"
+                    }
+                ]
+            }
+        }
 
 movies = [
     {
@@ -45,65 +73,71 @@ movies = [
 
     },
 ]
+class JWTBearer(HTTPBearer):
+    async def __call__(self, request: Request):
+        auth = await super().__call__(request)
+        data = validate_token(auth.credentials)
+        if data['email'] != "admin@gmail.com":
+            raise HTTPException(status_code=401, detail="Invalid user")
+        #return 
 
+#Primer End Point
 @app.get("/", tags=["Home"])
 def message():
     return HTMLResponse(
-        content="<h1>Mi primera chamba</h1>"
+        content="<h1>Bienvenido a mi Super Api</h1>"
     )
 
-@app.get("/movies", tags=["movies"], response_model=List[Movie])
-def get_movies():
+@app.get("/movies", tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
+def get_movies() -> List[Movie]:
     return JSONResponse(status_code=200, content=movies)
 
-@app.get("/movies/{id}", tags=["movies"], response_model=Movie)
-def get_movies_by_id(id: int):
-    for movie in movies:
-        if movie ["id"] == id:
-            founded_movie = movie
-    if not founded_movie:
-        return JSONResponse(content={"message":"Movie Not Found"}, status_code=404)
+#Segundo End Point
+@app.get("/movies/{id}", tags=['movies'], response_model=Movie, status_code=200)
+def get_movie(id: int = Path(ge=1, le=2000)) -> Movie: # se cambia y se dice que es igual a Path(int)
+    movie = list(filter(lambda movie: movie['id'] == id, movies))
+    if len(movie) > 0:
+        result = JSONResponse(content=movie, status_code=200)
     else:
-        return JSONResponse(content=founded_movie, status_code=200)
-    
+        result = JSONResponse(content={"message": "Movie not found"}, status_code=404)
+    return result
 
-@app.get("/movies/", tags=["movies"])
-def get_movies_by_category(category: str):
-    movies_filter = [movie for movie in movies if movie ["category"] == category]
-    if movies_filter == []:
-        return JSONResponse(content={"message":"Movie Not Found"}, status_code=404)
-    else:
-        return JSONResponse(content=movies_filter, status_code=200)
-    
+#Tercer End Point
+@app.get("/movies/", tags=['movies'], response_model=List[Movie])
+def get_movies_by_category(category: str = Query(min_length=3, max_length=15)) -> List[Movie]:
+    movies_by_category = [movie for movie in movies if movie['category'] == category]
+    return JSONResponse(content=movies_by_category)
 
+#Cuarto End Point
+@app.post("/movies", tags=['movies'], response_model=dict, status_code=201)
+def create_movie(movie: Movie) -> dict:
+    movies.append(movie)
+    return JSONResponse(content={"message": "Movie created successfully"}, status_code=201)
 
-@app.post("/movies", tags=["movies"])
-def add_movie(id:int = Body(),title:str = Body(),overview:str = Body(),year:int = Body(),rating:float = Body(),category:str = Body() ):
-    movies.append(
-        {"id" : id,
-         "title" : title,
-         "overview" : overview,
-         "year" : year,
-         "rating" : rating,
-         "category" : category
-         }
-    )
-    return movies
-    
-@app.put("/movies/{id}", tags=["movies"])
-def update_movie(id:int,title:str = Body(),overview:str = Body(),year:int = Body(),rating:float = Body(),category:str = Body() ):
+@app.put("/movies/{id}", tags=['movies'], response_model=dict, status_code=200)
+def update_movie(id: int, movie: Movie) -> dict:
     for movie in movies:
-        if movie ["id"] == id:
-            movie["title"] = title
-            movie["overview"] = overview
-            movie["year"] = year
-            movie["rating"] = rating
-            movie["category"] = category
-            return movies
-        
-@app.delete("movies/{id}", tags=["movies"])
-def delete_movie(id:int):
+        if movie['id'] == id:
+            movie['title'] = movie.title
+            movie['overview'] = movie.overview
+            movie['year'] = movie.year
+            movie['rating'] = movie.rating
+            movie['category'] = movie.category
+    return JSONResponse(content={"message": "Movie updated successfully"}, status_code=200)
+
+@app.delete("/movies/{id}", tags=['movies'], response_model=dict)
+def delete_movie(id: int) -> dict:
     for movie in movies:
-        if movie ["id"] == id:
+        if movie['id'] == id:
             movies.remove(movie)
-            return movies
+    return JSONResponse(content={"message": "Movie deleted successfully"})
+
+@app.post("/login", tags=['auth'], response_model=dict, status_code=200)
+def login(user: User) -> dict:
+    if user.email == "admin@gmail.com" and user.password == "admin":
+        token = create_token(data=user.model_dump())
+        return JSONResponse(content={"token": token}, 
+                            status_code=200)
+    else:
+        return JSONResponse(content={"message": "Invalid credentials"},
+                            status_code=401)
